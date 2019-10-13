4e3ab030e313b1155e07d8725c4c35301e329bcc
dianbero <dianbero@gmail.com>
2019-10-13 04:17:41 +0200

    Fecha validada y muestra sólo hora

--- 07-CombinarLayouts/07-CombinarLayouts/MainPage.xaml.cs
+++ 07-CombinarLayouts/07-CombinarLayouts/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
 
 namespace _07_CombinarLayouts
@@ -25,6 +26,57 @@ namespace _07_CombinarLayouts
         public MainPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            clsPersona objPersona = new clsPersona();
+
+            objPersona.Nombre = txtName.Text;
+            objPersona.Apellido = txtSurname.Text;
+            //objPersona.DateBirth = txtDateBirth.Text;
+
+            DateTime dateBirth;
+            
+            if (String.IsNullOrEmpty(txtDateBirth.Text))
+            {
+                lblDateBirth.Text = "Debe introducir una fecha";                
+            }else if(DateTime.TryParse(txtDateBirth.Text, out dateBirth))
+            {
+                objPersona.DateBirth = dateBirth;
+                lblDateBirth.Text = "";
+            }
+            
+
+            //objPersona.DateBirth = txtDateBirth;  //Hacer conversión, probar TryParse
+
+            if (String.IsNullOrWhiteSpace(objPersona.Nombre))
+            {
+                lblName.Text = "Debe introducir el nombre";
+            }
+            else
+            {
+                lblName.Text = "";
+            }
+            if (String.IsNullOrWhiteSpace(objPersona.Apellido))
+            {
+                lblSurname.Text = "Debe introducir el apellido";
+            }
+            else
+            {
+                lblSurname.Text = "";
+            }
+            /*if (String.IsNullOrWhiteSpace(objPersona.DateBirth))
+            {
+                lblDateBirth.Text = "Debe introducir una fecha";
+            }
+            else
+            {
+                lblDateBirth.Text = "";
+            }*/
+
+            lblResultadoFinal.Text = $"{objPersona.Nombre}, {objPersona.Apellido}, {objPersona.DateBirth.ToString("dd / MM / yyyy")}";  //Para mostrar sólo fecha no sirve .Date
+            
         }
     }
 }
---
+++ 07-CombinarLayouts/07-CombinarLayouts/Models/clsPersona.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_CombinarLayouts
+{
+    public class clsPersona
+    {
+        //Atributos privados
+        private string nombre;
+        //private int edad; //Es propiedad autoimplementada por lo que no se declara como private
+        private DateTime dateBirth;
+        //private string dateOfBirth;
+
+
+        //Constructor por defecto
+        public clsPersona()
+        {
+            /*this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Edad = edad;*/
+        }
+        //Constructor por parámetros
+        public clsPersona(string name, string surname, int age)
+        {
+            this.Nombre = name;
+            this.Apellido = surname;
+            this.Edad = age;
+                        
+        }
+
+        //Propiedades públicas
+        public string Nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+
+        public string Apellido { get; set; } //Equivale a lo anterior pero de forma más compacta, y no se declara previamente como private
+        public int Edad { get; set; }
+
+        
+        public DateTime DateBirth
+        {
+            get
+            {
+                return dateBirth;
+            }
+            set
+            {
+                dateBirth = value;
+            }
+        }
+
+        /*public DateTime DateBirth
+        {
+            get
+            {
+                return dateBirth;
+            }
+            set
+            {
+                if (DateTime.TryParse(dateOfBirth, out dateBirth))
+                {
+                    dateBirth = value;
+                }
+                else
+                {
+                    //dateOfBirth = null;
+                    dateBirth = value;
+                }          
+            }
+        }*/
+
+    }
+
+}
