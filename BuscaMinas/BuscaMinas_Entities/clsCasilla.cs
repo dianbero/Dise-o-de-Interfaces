0f27d5d78cfc72e066d0385f5fa3838b1aac892e
Diana <dianbero@gmail.com>
2019-11-04 14:38:15 +0100

    disaster

---
+++ BuscaMinas/BuscaMinas_Entities/clsCasilla.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscaMinas_Entities
+{
+    class clsCasilla : INotifyPropertyChanged
+    {
+        //Atributos privados
+        public bool esBomba;
+        private string imagenMostrada;
+        public int posicion;
+
+        public clsCasilla()
+        {
+            this.esBomba = false;
+            this.imagenMostrada = "Assets\\Imagenes\\presionar.png"; //Imagen que se muestra al principio
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string ImagenMostrada
+        {
+            get
+            {
+                //cambiarImagen();
+                return imagenMostrada;
+            }
+            set
+            {
+
+            }
+        }
+        public bool esbomba
+        {
+            get
+            {
+                //Con una sola imagen, cambiar imagen en get
+                return esbomba;
+            }
+            set
+            {
+                esBomba = value;
+            }
+        }
+    }
+}
---
+++ BuscaMinas/BuscaMinas_UI/Utilidades/clsListadoCasillas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuscaMinas_UI.Utilidades
+{
+    class clsListadoCasillas
+    {
+        public List<clsListadoCasillas> ListadoCompletoCasillas()
+        {
+            List<clsListadoCasillas> listadoCasillas = new List<clsListadoCasillas>();
+            listadoCasillas.Add(new clsListadoCasillas());
+
+            return listadoCasillas;
+        }
+    }
+}
