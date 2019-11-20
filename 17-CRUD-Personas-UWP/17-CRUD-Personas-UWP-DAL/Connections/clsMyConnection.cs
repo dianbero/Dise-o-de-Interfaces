23c55410e92d3285e1eac574d928c6c5062e1758
Diana <dianbero@gmail.com>
2019-11-20 11:13:51 +0100

    sdf

--- 17-CRUD-Personas-UWP/17-CRUD-Personas-UWP-DAL/Connections/clsMyConnection.cs
+++ 17-CRUD-Personas-UWP/17-CRUD-Personas-UWP-DAL/Connections/clsMyConnection.cs
@@ -35,12 +35,20 @@ namespace _17_CRUD_Personas_UWP_DAL.Connections
         public clsMyConnection()
         {
             // this.server = "(local)";
-            this.server = "dianbero.database.windows.net";
+            //this.server = "dianbero.database.windows.net";
+            //this.dataBase = "PersonasDB";
+            //this.user = "dianbero";
+            ////this.pass = "123";
+            //this.pass = "Mi.Tesoro"; 
 
+            //this.server = "(local)";
+            this.server = "pablo90.database.windows.net";
             this.dataBase = "PersonasDB";
-            this.user = "dianbero";
+            this.user = "pablo";
             //this.pass = "123";
-            this.pass = "Mi.Tesoro";
+            this.pass = "Mitesoro123";
+
+
 
         }
         //Con parámetros por si quisiera cambiar las conexiones
