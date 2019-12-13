25b5262abc50d69a15ac2d0563b391b31c7f4f1a
Diana <dianbero@gmail.com>
2019-12-13 14:37:38 +0100

    creados proyectos Minijuego Topitos

---
+++ MiniJuegoTopitos/MiniJuegoTopitosServer/MiniJuegoTopitosServer/TopitosHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace MiniJuegoTopitosServer
+{
+    public class TopitosHub : Hub
+    {
+        //TODO modificar para topitos
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
