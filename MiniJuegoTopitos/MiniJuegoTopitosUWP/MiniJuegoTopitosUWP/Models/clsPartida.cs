1612b8c2abaf6ab9ec466ce85adb25463a58765c
Diana <dianbero@gmail.com>
2020-01-08 11:09:18 +0100

    cambios chorras

--- MiniJuegoTopitos/MiniJuegoTopitosUWP/MiniJuegoTopitosUWP/Models/clsPartida.cs
+++ MiniJuegoTopitos/MiniJuegoTopitosUWP/MiniJuegoTopitosUWP/Models/clsPartida.cs
@@ -11,10 +11,16 @@ namespace MiniJuegoTopitosUWP.Models
     {
         /*Funciones:
          - Calcular puntos de jugadores
-         -Calcular tiempo en que aparace topo
+         - Calcular tiempo en que aparace topo
          -*/
         private static Timer timer;
 
+        /// <summary>
+        /// Método que suma un punto al gugador ganador del turno
+        /// </summary>
+        /// <param name="puntos"></param>
+        /// <param name="isGanadorTurno"></param>
+        /// <returns></returns>
         public static int sumarPuntos(int puntos, bool isGanadorTurno)
         {            
             if (isGanadorTurno)
@@ -36,6 +42,10 @@ namespace MiniJuegoTopitosUWP.Models
         //    int posicion = random.Next(17); //Randoms de 0 - 16
         //}
 
+        /// <summary>
+        /// Método que genera un número aleatorio para la posición del todo en la casilla.
+        /// </summary>
+        /// <returns></returns>
         public static int asignarPosicionTopo()
         {
             Random random = new Random();
