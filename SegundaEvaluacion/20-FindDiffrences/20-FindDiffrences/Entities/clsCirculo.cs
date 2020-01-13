e9d6d3aaf33eaf702433ca33de9c8895782ef425
dianbero <dianbero@gmail.com>
2020-01-13 01:43:29 +0100

    nanai

---
+++ SegundaEvaluacion/20-FindDiffrences/20-FindDiffrences/Entities/clsCirculo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+namespace _20_FindDiffrences.Entities
+{
+    public class clsCirculo
+    {
+        #region Propiedades Públicas
+        public bool IsSeleccionado { get; set; }
+        //public bool IsVisible { get; set; }
+        public double EjeX { get; set; }
+        public double EjeY { get; set; }
+        public int Diametro { get; set; }
+        public Ellipse Circulo { get; set; }
+        #endregion
+
+
+        #region Constructor
+        //Constructor por defecto
+        public clsCirculo()
+        {
+            EjeX = 0;
+            EjeY = 0;
+            Diametro = 60;            
+            IsSeleccionado = false;
+            //IsVisible = false;
+            Circulo = asignarPropiedadesEllipse(Diametro, IsSeleccionado);            
+        }
+        public clsCirculo(double x, double y)
+        {
+            EjeX = x;
+            EjeY = y;
+            Diametro = 60;
+            IsSeleccionado = false;
+            //IsVisible = false;
+            Circulo = asignarPropiedadesEllipse(Diametro, IsSeleccionado);
+        }
+        #endregion
+
+        #region Métodos
+        public Ellipse asignarPropiedadesEllipse(int diametro, bool seleccionado)
+        {
+            Ellipse circulo = new Ellipse();
+            circulo.Width = diametro;
+            circulo.Height = diametro;
+            circulo.Stroke = new SolidColorBrush(Colors.Black);
+            circulo.StrokeThickness = 5;
+            if (seleccionado)
+            {
+                circulo.Opacity = 1;
+            }
+            else
+            {
+                circulo.Opacity = 0;
+            }
+            return circulo;
+        }
+
+        public void posicionEnImagen()
+        {
+            Canvas.SetLeft(Circulo, EjeX);
+            Canvas.SetTop(Circulo, EjeY);
+        }
+        #endregion
+    }
+}
---
+++ SegundaEvaluacion/20-FindDiffrences/20-FindDiffrences/ViewModels/clsMainPageVM.cs
@@ -0,0 +1,55 @@
+﻿using _20_FindDiffrences.Entities;
+using _20_FindDiffrences.Utiles;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _20_FindDiffrences.ViewModels
+{
+    public class clsMainPageVM : clsVMBase
+    {
+        #region Atributos Privados
+        private clsCirculo circuloSeleccionado;
+        private ObservableCollection<clsCirculo> listaCirculos;
+        #endregion
+        #region Propiedades Públicas
+        public clsCirculo CirculoSeleccionado
+        {
+            get
+            {
+                return circuloSeleccionado;
+            }
+            set
+            {
+                circuloSeleccionado = value;
+                NotifyPropertyChanged("CirculoSeleccionado");
+            }
+        }
+
+        public ObservableCollection<clsCirculo> ListaCirculos
+        {
+            get
+            {
+                return listaCirculos;
+            }
+            set
+            {
+                listaCirculos = value;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        public clsMainPageVM()
+        {
+            listaCirculos = clsListaCirculos.listaCirculos();
+
+        }
+        #endregion
+
+
+    }
+}
