e231eb2538982af8d17632c0ee2fb15e201166b0
Diana <dianbero@gmail.com>
2020-01-27 14:41:06 +0100

    xamarin

---
+++ Xamarin/PruebaXamarin/PruebaXamarin/PruebaXamarin/MainPageVM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace PruebaXamarin
+{
+    public class MainPageVM : clsVMBase
+    {
+        private string name;
+
+        public string Name
+        {
+            get => name;
+            set => NotifyPropertyChanged("Name");
+        }
+    
+        public ICommand EnterCommand => new Command(EnterCommandExecute);
+
+        public async void EnterCommandExecute()
+        {
+            if (string.IsNullOrWhiteSpace(Name))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Alert",
+                    "You have been alerted",
+                    "OK");
+                return;
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
